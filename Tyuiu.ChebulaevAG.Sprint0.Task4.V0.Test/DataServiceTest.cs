ecef57bb47300c41ba70c6897241ea5aed9d79e8
Tyuiu.ChebulaevAG@outlook.com <andrey.chebulaev@gmail.com>
2023-09-25 20:11:46 +0500

    Task4

---
+++ Tyuiu.ChebulaevAG.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ChebulaevAG.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.ChebulaevAG.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedDevisionValid()
+        {
+            Assert.AreEqual(3, DataService.Devision(9, 3));
+        }
+    }
+}
